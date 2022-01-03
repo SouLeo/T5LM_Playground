f36a8f7917d525bcb23754d04e9fd8b9d73776ae
SouLeo <slwanna@utexas.edu>
2022-01-03 11:58:49 -0700

    reorg, added gpu capabilities

--- main.py
+++ main.py
@@ -2,41 +2,19 @@ import torch
 import transformers
 import data_input
 import t5_prompt_creation
-
-
-def check_sequence_len(max_len: int, inputs: list):
-    # Method 1: Limit character len
-    inputs = [e[:max_len] for e in inputs]
-    # Method 2: Limit token len
-    # i = 0
-    # for item in inputs:
-    #     num_tokens = len(item.split())
-    #     print('old length ' + str(num_tokens))
-    #     if num_tokens > max_len:
-    #         cropped_input = item.split()[:max_len]
-    #         print('new length ' + str(len(cropped_input)))
-    #         item[i] = ' '.join(cropped_input)
-    #     i = i + 1
-    return inputs
-
-
-def add_padding(examples: list):
-    # The code below tests the max sequence length of my data.
-    # The max is 254, so I manually set the padding to 255
-    #
-    # max_val = 0
-    # for item in examples:
-    #     if len(item) > max_val:
-    #         max_val = len(item)
-    #
-    max_val = 255
-    for ex in examples:
-        ex.extend([0] * (max_val - len(ex)))
-    return examples
+import helper_functions
 
 
 if __name__ == '__main__':
-    print('you are running big main')
+    print('you are running the training program')
+
+    # checking if gpu can be used in training
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print('gpu found')
+    else:
+        device = torch.device('cpu')
+        print('using cpu')
 
     # Collecting Data
     datainput = data_input.DataInput()
@@ -55,18 +33,18 @@ if __name__ == '__main__':
 
     # verifying sequence lengths are in good standing w/ model constraints
     max_seq_len = 512  # Design constraint for t5-small model
-    inputs = check_sequence_len(max_seq_len, inputs)
-    training_labels = check_sequence_len(max_seq_len, training_labels)
+    inputs = helper_functions.check_sequence_len(max_seq_len, inputs)
+    training_labels = helper_functions.check_sequence_len(max_seq_len, training_labels)
     # end of sequence length checks
 
     # model setup
     tokenizer = transformers.T5Tokenizer.from_pretrained('t5-small')
     model = transformers.AutoModel.from_pretrained("google/t5-small-lm-adapt")
-    inputs_encoded, attention_mask = torch.tensor(tokenizer(inputs).data['input_ids']), tokenizer(inputs).data['attention_mask'],
-    labels_encoded = torch.tensor(add_padding(tokenizer(training_labels).data['input_ids']))
+    model = model.to(device)
+    inputs_encoded, attention_mask = torch.tensor(tokenizer(inputs).data['input_ids']).to(device), torch.tensor(tokenizer(inputs).data['attention_mask']).to(device),
+    labels_encoded = torch.tensor(helper_functions.add_padding(tokenizer(training_labels).data['input_ids']))
     labels_encoded[labels_encoded == tokenizer.pad_token_id] = -100
 
-    loss = model(input_ids=inputs_encoded).loss
+    loss = model(input_ids=inputs_encoded).forward
     print(loss)
     print('end')
-
